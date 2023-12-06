60eaeb4f42ae367c2edba9278ceb8dddd00c6adf
Serhii Babanov <serhii.babanov@gmail.com>
2023-12-05 22:15:59 -0500

    corrected resource rel

--- src/main/java/com/candles/demo/repository/EmailSubscriptionRepository.java
+++ src/main/java/com/candles/demo/repository/EmailSubscriptionRepository.java
@@ -4,6 +4,6 @@ import com.candles.demo.model.EmailSubscription;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource(collectionResourceRel = "candles", path = "candles")
+@RepositoryRestResource(collectionResourceRel = "subscription", path = "subscription")
 public interface EmailSubscriptionRepository extends MongoRepository<EmailSubscription, String> {
 }
