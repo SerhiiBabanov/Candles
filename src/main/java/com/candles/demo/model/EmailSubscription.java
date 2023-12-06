81840e2c4973151b80f54ada69614dda1bae155f
Serhii Babanov <serhii.babanov@gmail.com>
2023-12-05 22:02:57 -0500

    add Id for email. delete annotation from Aroma

--- src/main/java/com/candles/demo/model/EmailSubscription.java
+++ src/main/java/com/candles/demo/model/EmailSubscription.java
@@ -2,9 +2,11 @@ package com.candles.demo.model;
 
 import jakarta.validation.constraints.Email;
 import lombok.Data;
+import org.springframework.data.annotation.Id;
 
 @Data
 public class EmailSubscription {
+    @Id
     @Email
     private String email;
 }
