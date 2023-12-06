68bd702e4896a9ad69c601fe891cdd30df67d0d3
Serhii Babanov <serhii.babanov@gmail.com>
2023-12-05 22:27:07 -0500

    delete annotation Id

--- src/main/java/com/candles/demo/model/EmailSubscription.java
+++ src/main/java/com/candles/demo/model/EmailSubscription.java
@@ -5,13 +5,11 @@ import jakarta.validation.constraints.Email;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
 @JsonSerialize
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class EmailSubscription {
-    @Id
     @Email
     private String email;
 }
