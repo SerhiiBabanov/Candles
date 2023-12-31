a6e767579d265de9bd5d9d3018661e8a0731399e
Serhii Babanov <serhii.babanov@gmail.com>
2023-12-31 16:54:46 -0500

    add api for load and download all candles and boxes from db via xlsx file

--- src/main/java/com/candles/features/loadDataInDb/WrongFileFormatException.java
+++
@@ -1,7 +0,0 @@
-package com.candles.features.loadDataInDb;
-
-public class WrongFileFormatException extends RuntimeException {
-    public WrongFileFormatException(String message) {
-        super(message);
-    }
-}
