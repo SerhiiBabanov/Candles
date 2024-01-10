5449f2f9b7f6b68a440fcf32ee1333e8057ac863
Serhii Babanov <63935958+SerhiiBabanov@users.noreply.github.com>
2024-01-09 19:29:32 -0500

    Telegram notification (#22)
    * add telegram notification about placed order

---
+++ src/main/java/com/candles/features/orderNotification/telegram/TelegramNotifierService.java
@@ -0,0 +1,46 @@
+package com.candles.features.orderNotification.telegram;
+
+import com.candles.features.order.Order;
+import jakarta.ws.rs.core.UriBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
+
+@Service
+public class TelegramNotifierService {
+    private final String CHAT_ID;
+    private final String TOKEN;
+
+    public TelegramNotifierService(@Value("${telegram.bot.token}") String TOKEN,
+                                   @Value("${telegram.bot.chat.id}") String CHAT_ID) {
+        this.CHAT_ID = CHAT_ID;
+        this.TOKEN = TOKEN;
+    }
+
+    public void sendOrderNotification(Order order) throws IOException, InterruptedException {
+        TelegramOrderMessage message = new TelegramOrderMessage(order);
+        HttpClient client = HttpClient.newBuilder()
+                .connectTimeout(Duration.ofSeconds(5))
+                .version(HttpClient.Version.HTTP_2)
+                .build();
+
+        UriBuilder builder = UriBuilder
+                .fromUri("https://api.telegram.org")
+                .path("/{token}/sendMessage")
+                .queryParam("chat_id", CHAT_ID)
+                .queryParam("text", message.toString());
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .GET()
+                .uri(builder.build("bot" + TOKEN))
+                .timeout(Duration.ofSeconds(5))
+                .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+    }
+}
---
+++ src/main/java/com/candles/features/orderNotification/telegram/TelegramOrderMessage.java
@@ -0,0 +1,31 @@
+package com.candles.features.orderNotification.telegram;
+
+import com.candles.features.order.Order;
+import lombok.AllArgsConstructor;
+
+import java.util.stream.Collectors;
+
+@AllArgsConstructor
+public class TelegramOrderMessage {
+    private final Order order;
+
+    @Override
+    public String toString() {
+        return "Order #" + order.getId() + ":\n" +
+                "customer= " + order.getCustomer().getFullName() + "\n" +
+                "phone= " + order.getCustomer().getPhone() + "\n" +
+                "email= " + order.getCustomer().getEmail() + "\n" +
+                "address= " + order.getCustomer().getAddress() + "\n" +
+                "comment= " + order.getCustomer().getComment() + "\n" +
+                "items= " + "\n" +
+                order.getItems().stream()
+                        .map(item -> "  " + item.getName() + " - quantity:" + item.getQuantity())
+                        .collect(Collectors.joining("\n"))
+                + "\n" +
+                order.getCustomCandles().stream()
+                        .map(item -> "  " + "Custom candle " + " - quantity:" + item.getQuantity())
+                        .collect(Collectors.joining("\n"))
+                + "\n" +
+                "total price=" + order.getTotal();
+    }
+}
